0cda60cd06c718df2fd6e1debe863f6d795b3f68
loudtrexx <pelisahko99@gmail.com>
2025-02-20 08:10:52 +0200

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   Start_OS_check.py

---
+++ Start_OS_check.py
@@ -0,0 +1,34 @@
+import platform
+
+# OS check for the start of the program
+def OS_chk():
+    # Get the os and its version
+    os = platform.system()
+    ver = platform.version()
+
+# Output the result
+    try:
+        if os == "Windows":
+            print(f"You are running Windows {ver}. The script is made for GNU/Linux!")
+            input("Press Enter to exit ")
+            exit()
+    
+        elif os == "Darwin":
+            print(f"You are running {os} (Likely MacOS). This script is made for GNU/Linux!")
+            input("Press Enter to exit ")
+            exit()
+    
+    
+        elif os == "Linux":
+            print(f"Running {os} {ver}")
+            pass
+
+        else:
+            print("OS is unknown (Like non-linux UNIX or ReactOS) rerun the script if you believe this is a mistake")
+            input("Press Enter to exit ")
+            exit()
+    except KeyboardInterrupt:
+        print("Ended by the user")
+        exit()
+
+OS_chk() 
