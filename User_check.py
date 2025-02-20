8fa4b808fda62c689542918c343bfe4e2229f39b
loudtrexx <pelisahko99@gmail.com>
2025-02-20 12:13:30 +0200

    On branch main Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   User_check.py

--- User_check.py
+++ User_check.py
@@ -1,16 +1,38 @@
-import os
+import subprocess
 
 def usr_check():
     try:
-        user = os.system("whoami")
+        user = subprocess.run(["whoami"],capture_output=True,text=True)
+        user = user.stdout.strip()
         print(user)
         if user == "root":
             print("User ok!")
-            os.system("mkdir /root/test")
+            root_check()
+            
         
         else:
             print("Not running as root")
+            exit()
+    except KeyboardInterrupt:
+        print("Ended by the user")
+
+def root_check():
+    try:
+                subprocess.run(["mkdir", "/root/test"], check=True, capture_output=True, text=True)
+                subprocess.run(["rm", "-fr", "/root/test"], check=True)
+                print("Root check was successful!")
+            
+    except subprocess.CalledProcessError as e:
+        print(e.stderr)
+        if "File exists" in e.stderr:
+            print("Yes")
+            subprocess.run(["mv", "/root/test/", "test1"])
+            print("Your old 'test' folder in '/root' was renamed to 'test1'. The script will try again.")
+            root_check()
+        elif "Permission denied":
+            print("Failed to write to '/root'. Are you fakerooted?")
     except KeyboardInterrupt:
         print("Ended by the user")
 
 usr_check()
+root_check()
