d2acef85b2ad30f798e47fd6d4045d2bcb9f67a7
loudtrexx <pelisahko99@gmail.com>
2025-02-21 08:50:36 +0200

    Updated Menu.py
    - Modified the manual option to open the github wiki or the project instead
    - Added some comments for couple of lines

--- Menu.py
+++ Menu.py
@@ -6,27 +6,29 @@ def choices():
         print("Select one of the options:")
         print("1. Check for windows installations")
         print("2. project github")
-        print("3. Manual (Coming soon!)")
+        print("3. Read the project wiki or edit it!")
         print("4. Exit")
         choose = input()
-        if choose == "1":
+        if choose == "1": # Begin the real deal when it is done
             print("Function coming soon!")
     
-        elif choose == "2":
+        elif choose == "2": # Open github page (Duh)
             webbrowser.open("github.com/loudtrexx/Win-to-linux-merge")
             print("Github opened!")
             choices()
     
         elif choose == "3":
-            print("Function coming soon!")
+            webbrowser.open("https://github.com/loudtrexx/Win-to-linux-merge/wiki/")
+            print("Opening wiki...")
+            choices()
         
         elif choose == "4":
             print("Closing...")
             exit()
     
-        else:
+        else: # Notify user that input is invalid
             input(f"{choose} is not a valid choice! Continue with enter")
-            os.system("cls")
+            os.system("clear")
             choices()
     # Handle Keyboard interrupts and TypeErrors
     except KeyboardInterrupt:
