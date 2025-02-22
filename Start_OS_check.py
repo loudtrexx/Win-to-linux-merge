f501bbf5872a28796f2ced6c588548085ac29cb6
loudtrexx <pelisahko99@gmail.com>
2025-02-22 21:44:33 +0200

    Updated Start_OS_check.py
    Removed separate lines for other operating systems to make the code cleaner looking.

--- Start_OS_check.py
+++ Start_OS_check.py
@@ -8,23 +8,15 @@ def OS_chk():
 
 # Output the result
     try:
-        if os == "Windows":
-            print(f"You are running Windows {ver}. The script is made for GNU/Linux!")
-            input("Press Enter to exit ")
-            exit()
-    
-        elif os == "Darwin":
-            print(f"You are running {os} (Likely MacOS). This script is made for GNU/Linux!")
-            input("Press Enter to exit ")
-            exit()
+
     
     
-        elif os == "Linux":
+        if os == "Linux":
             print(f"Running {os} {ver}")
             pass
 
         else:
-            print("OS is not recognized. It could be because the current os is non-linux UNIX or ReactOS. Rerun the script if you believe this is a mistake")
+            print(f"You are running {os} {ver}. Program is made for GNU/Linux")
             input("Press Enter to exit ")
             exit()
     except KeyboardInterrupt:
