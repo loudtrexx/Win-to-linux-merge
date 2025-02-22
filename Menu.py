d57816d2fe9723678272e972a3c184fc28561a97
loudtrexx <pelisahko99@gmail.com>
2025-02-22 21:41:51 +0200

    Add files via upload
    - Added new Advanced/debugging option for the future.
    - Modified some text to look more in place with the others.

--- Menu.py
+++ Menu.py
@@ -5,25 +5,30 @@ def choices():
     try:
         print("Select one of the options:")
         print("1. Check for windows installations")
-        print("2. project github")
+        print("2. Project github")
         print("3. Read the project wiki or edit it!")
-        print("4. Exit")
+        print("4. Advanced/debugging features")
+        print("5. Exit")
         choose = input()
+        os.system("clear")
         if choose == "1": # Begin the real deal when it is done
             print("Function coming soon!")
     
         elif choose == "2": # Open github page (Duh)
-            webbrowser.open("github.com/loudtrexx/Win-to-linux-merge")
-            print("Github opened!")
+            webbrowser.open("https://github.com/loudtrexx/Win-to-linux-merge")
+            print("Opening GitHub...")
             choices()
     
-        elif choose == "3":
+        elif choose == "3": # Open the wiki with useful information and return to the menu
             webbrowser.open("https://github.com/loudtrexx/Win-to-linux-merge/wiki/")
             print("Opening wiki...")
             choices()
         
-        elif choose == "4":
-            print("Closing...")
+        elif choose == "4": # Call settings page function
+            print("Not yet available")
+            choices()
+        
+        elif choose == "5":
             exit()
     
         else: # Notify user that input is invalid
