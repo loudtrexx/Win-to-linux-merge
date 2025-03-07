15b65974fb14f6ac1b642b10b6418cf13acd98c1
loudtrexx <pelisahko99@gmail.com>
2025-03-07 12:10:15 +0200

    Updated Start_OS_check.py
    - Fixed script causing other scripts to be ignored or overlap causing weird outcomes

--- Start_OS_check.py
+++ Start_OS_check.py
@@ -2,25 +2,19 @@ import platform
 
 # OS check for the start of the program
 def OS_chk():
-    # Get the os and its version
-    os = platform.system()
-    ver = platform.version()
-
-# Output the result
-    try:
-
-    
-    
+    checked = False
+    while checked == False: 
+        # Get the os and its version
+        os = platform.system()
+        ver = platform.version()
         if os == "Linux":
             print(f"Running {os} {ver}")
-            pass
+            checked = True
+            continue
 
         else:
             print(f"You are running {os} {ver}. Program is made for GNU/Linux")
             input("Press Enter to exit ")
-            exit()
-    except KeyboardInterrupt:
-        print("Ended by the user")
-        exit()
+            break
 
-OS_chk() 
+#OS_chk() 
