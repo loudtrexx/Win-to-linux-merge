90fe322aed2ddd1f68a071fd3cc94385d8238e4b
loudtrexx <pelisahko99@gmail.com>
2025-03-07 12:06:58 +0200

    Modified Menu.py
    - Changed the first option to call the now existing function
    - Fixed the script running itself and causing some commands to be sometimes ignored

--- Menu.py
+++ Menu.py
@@ -1,8 +1,10 @@
 # os for running terminal commands during the script, webbrowser to open github
-import webbrowser, os
+import webbrowser, os, mergement
+
 
 def choices():
     try:
+
         print("Select one of the options:")
         print("1. Check for windows installations")
         print("2. Project github")
@@ -11,35 +13,32 @@ def choices():
         print("5. Exit")
         choose = input()
         os.system("clear")
-        if choose == "1": # Begin the real deal when it is done
-            print("Function coming soon!")
-    
+        if choose == "1": # Begin the real deal
+                mergement.compilation()
+            
         elif choose == "2": # Open github page (Duh)
             webbrowser.open("https://github.com/loudtrexx/Win-to-linux-merge")
             print("Opening GitHub...")
             choices()
-    
+            
         elif choose == "3": # Open the wiki with useful information and return to the menu
             webbrowser.open("https://github.com/loudtrexx/Win-to-linux-merge/wiki/")
             print("Opening wiki...")
             choices()
-        
+                
         elif choose == "4": # Call settings page function
             print("Not yet available")
             choices()
-        
+                
         elif choose == "5":
+            print("Bye bye!")
             exit()
-    
+            
         else: # Notify user that input is invalid
-            input(f"{choose} is not a valid choice! Continue with enter")
+            input(f"{choose} is not a valid choice! Continue with enter ")
             os.system("clear")
             choices()
-    # Handle Keyboard interrupts and TypeErrors
     except KeyboardInterrupt:
-        print("Ended by the user")
-    except TypeError:
-        input("Something went wrong... Continue with Enter:")
-        choices()
+        print("Ended by the user.")
 
-choices()
+#choices()
